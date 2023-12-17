ca38addc485f06ea0aae90d28ed791383cfdef41
Sergeeva Alina <alin.k4@yandex.ru>
2023-12-17 14:58:05 +0300

    first commit ads

--- 003-mvc/routes/web.php
+++ 003-mvc/routes/web.php
@@ -8,8 +8,8 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
 |
 */
 
@@ -17,10 +17,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::group(['middleware' => 'auth'], function () {
-    Route::get('/user', [\App\Http\Controllers\ProfileController::class, 'index'])->name('user');
-});
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/todo', [\App\Http\Controllers\TodoController::class, 'index'])->name('todo.index');
+Route::get('/todo/{id}', [\App\Http\Controllers\TodoController::class, 'show'])->where('id', '[0-9]+')->name('todo.show');
+Route::get('/todo/create', [\App\Http\Controllers\TodoController::class, 'create'])->name('todo.create');
